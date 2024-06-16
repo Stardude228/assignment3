a037c9b2f1faf23675d1c89e781ea662688812ad
Oomat Latipov <oomatlatipov@Oomats-MBP.lan>
2024-06-15 23:03:54 -0400

    Changes in maximum of a message

--- a3_caesar.c
+++ a3_caesar.c
@@ -32,7 +32,7 @@ void encryptMessage(char *message, int shift) {
 }
 
 int main() {
-    char message[350];
+    char message[250];
     int shift;
     int stop = 0; // Flag to control program termination
     
